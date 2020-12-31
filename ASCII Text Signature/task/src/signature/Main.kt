8f96755ffc8d8e80898cdb7ebb614d201a4bc9c8
Icasso <isaactsui2000@gmail.com>
2020-12-31 22:22:18 +0800

    Done Stage 3/4: Make it notable

--- ASCII Text Signature/task/src/signature/Main.kt
+++ ASCII Text Signature/task/src/signature/Main.kt
@@ -4,20 +4,81 @@ import java.util.*
 
 val scanner = Scanner(System.`in`)
 fun main() {
+    print("Enter name and surname: ")
     var name = scanner.nextLine()
+    print("Enter person's status: ")
+    val status = scanner.nextLine()
+    val statusSize = status.length
     name = name.toLowerCase()
-    val size = name.length
-    print("*".repeat(size + 4) + "\n")
-    println("* $name *")
-    print("*".repeat(size + 4) + "\n")
+
 
     name.toCharArray()
-    for (i in 0..2) {
-        for (char in name) {
-            print(returnFont(char)[i] + " ")
-        }
-        println()
+    //Count
+    var sum = 5
+    for (char in name) {
+        sum += returnFont(char)[0].length
+        sum++
     }
+
+    var isLonger = false
+    var printLen: Int
+    if (sum > statusSize + 6) {
+        printLen = sum
+        isLonger = false
+    } else {
+        printLen = statusSize + 6
+        isLonger = true
+    }
+//    println("sum: $sum")
+//    println("printLen: $printLen")
+    print("*".repeat(printLen) + "\n")
+
+    if (!isLonger) {
+        for (i in 0..2) {
+            print("*  ")
+            for (char in name) {
+                print(returnFont(char)[i] + " ")
+            }
+            println(" *")
+        }
+    } else {
+        var letterLen = printLen - sum
+//        println(letterLen)
+        if (letterLen % 2 != 0) {
+            val v1 = ((letterLen / 2.0) - 0.5).toInt()
+            val v2 = ((letterLen / 2.0) + 0.5).toInt()
+            for (i in 0..2) {
+                print("*  " + " ".repeat(v1))
+                for (char in name) {
+                    print(returnFont(char)[i] + " ")
+                }
+                println(" ".repeat(v2) + " *")
+            }
+        } else {
+            val v1 = letterLen / 2
+            for (i in 0..2) {
+                print("*  " + " ".repeat(v1))
+                for (char in name) {
+                    print(returnFont(char)[i] + " ")
+                }
+                println(" ".repeat(v1) + " *")
+            }
+        }
+
+    }
+
+
+    val test = (printLen - 6 - statusSize) / 2.0
+    if (test * 2 % 2 != 0.0) {
+        val r1 = (test - 0.5).toInt()
+        val r2 = (test + 0.5).toInt()
+        println("*  " + " ".repeat(r1) + status + " ".repeat(r2) + "  *")
+    } else {
+        println("*  " + " ".repeat(test.toInt()) + status + " ".repeat(test.toInt()) + "  *")
+    }
+
+    print("*".repeat(printLen) + "\n")
+
 }
 
 fun returnFont(letter: Char): Array<String> {
@@ -48,7 +109,7 @@ fun returnFont(letter: Char): Array<String> {
         'x' -> (return arrayOf("_  _", " \\/ ", "_/\\_"))
         'y' -> (return arrayOf("_   _", " \\_/ ", "  |  "))
         'z' -> (return arrayOf("___ ", "  / ", " /__"))
-        ' ' -> (return arrayOf("      ", "      ", "      "))
+        ' ' -> (return arrayOf("    ", "    ", "    "))
         else -> return arrayOf("", "", "")
     }
 }
